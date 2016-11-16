1e7c588f934c06ef7bd891e96b7f773dfc48adca
ark dan <arkdan@icloud.com>
2016-11-16 22:31:05 +0200

    osx target

---
+++ ARKExtensions-macOS/ARKExtensions-macOS.h
@@ -0,0 +1,19 @@
+//
+//  ARKExtensions-macOS.h
+//  ARKExtensions-macOS
+//
+//  Created by ark dan on 11/16/16.
+//  Copyright © 2016 arkdan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ARKExtensions-macOS.
+FOUNDATION_EXPORT double ARKExtensions_macOSVersionNumber;
+
+//! Project version string for ARKExtensions-macOS.
+FOUNDATION_EXPORT const unsigned char ARKExtensions_macOSVersionString[];
+
+#import <ARKExtensions/NSTimer+Blocks.h>
+
+
