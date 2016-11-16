867e58c3428ad75182b95bac088665f6d889476b
ark dan <arkdan@icloud.com>
2016-11-16 21:40:13 +0200

    initial

---
+++ ARKExtensions/ARKExtensions.h
@@ -0,0 +1,19 @@
+//
+//  ARKExtensions.h
+//  ARKExtensions
+//
+//  Created by ark dan on 11/16/16.
+//  Copyright © 2016 arkdan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ARKExtensions.
+FOUNDATION_EXPORT double ARKExtensionsVersionNumber;
+
+//! Project version string for ARKExtensions.
+FOUNDATION_EXPORT const unsigned char ARKExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ARKExtensions/PublicHeader.h>
+
+
