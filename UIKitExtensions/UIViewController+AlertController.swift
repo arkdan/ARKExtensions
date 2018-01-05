4f01f662e5703c1357081fb4106e74fde057e57a
ark dan <arkdan@icloud.com>
2018-01-05 11:51:24 +0200

    AlertController returns a alert

--- UIKitExtensions/UIViewController+AlertController.swift
+++ UIKitExtensions/UIViewController+AlertController.swift
@@ -16,15 +16,51 @@ private let alertQueue: OperationQueue = {
     return queue
 }()
 
+private class AlertOperation: OOperation {
+
+    let alert: UIAlertController
+    var presentation: (UIAlertController) -> Void
+
+    init(title: String?, message: String?, cancelButtonTitle: String, otherButtonTitles: [String]?, handler: ((Int) -> Void)?, presentation: @escaping (UIAlertController) -> Void) {
+        self.alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        self.presentation = presentation
+        super.init()
+        let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { [weak self] alert in
+            handler?(0)
+            self?.finish()
+        }
+        self.alert.addAction(cancelAction)
+
+        if let titles = otherButtonTitles {
+            for (index, title) in titles.enumerated() {
+                let action = UIAlertAction(title: title, style: .default) { [weak self] (alert) -> Void in
+                    handler?(index + 1) // shifted by one because of 'cancel' action
+                    self?.finish()
+                }
+                alert.addAction(action)
+            }
+        }
+    }
+
+    override func execute() {
+        onMain {
+            self.presentation(self.alert)
+        }
+    }
+}
+
 extension UIViewController {
 
     /// Presented form window's rootViewController.
     /// Does nothing if no rootViewController
-    public class func presentAlert(title: String?, message: String?, cancelButtonTitle: String, otherButtonTitles:[String]? = nil, handler:((Int) -> ())? = nil) {
+    @discardableResult
+    public class func presentAlert(title: String?, message: String?, cancelButtonTitle: String, otherButtonTitles:[String]? = nil, handler:((Int) -> ())? = nil) -> UIAlertController? {
+
         if let rootVC = UIApplication.shared.delegate?.window??.rootViewController {
-            rootVC.presentAlert(title: title, message: message, cancelButtonTitle: cancelButtonTitle,
-                                otherButtonTitles: otherButtonTitles, handler: handler)
+            return rootVC.presentAlert(title: title, message: message, cancelButtonTitle: cancelButtonTitle,
+                                       otherButtonTitles: otherButtonTitles, handler: handler)
         }
+        return nil
     }
 
 
@@ -33,30 +69,38 @@ extension UIViewController {
     /// - Parameters:
     ///   - handler: callback when user taps either button (including Cancel).
     /// buttonIndex == 0 corresponds to Cancel; buttonIndex == 1 - first button from otherButtonTitles
-    public func presentAlert(title: String?, message: String?, cancelButtonTitle: String, otherButtonTitles: [String]?, handler: ((Int) -> ())?) {
+    @discardableResult
+    public func presentAlert(title: String?, message: String?, cancelButtonTitle: String, otherButtonTitles: [String]?, handler: ((Int) -> ())?) -> UIAlertController {
 
-        let op = OOperation { finished in
-            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-            let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { alert in
-                handler?(0)
-                finished()
-            }
-            alert.addAction(cancelAction)
-
-            if let titles = otherButtonTitles {
-                for (index, title) in titles.enumerated() {
-                    let action = UIAlertAction(title: title, style: .default) { (alert) -> Void in
-                        handler?(index + 1) // shifted by one because of 'cancel' action
-                        finished()
-                    }
-                    alert.addAction(action)
-                }
-            }
-
-            self.present(alert, animated: true, completion: nil)
+        let presentation: (UIAlertController) -> Void = { [weak self] alert in
+            self?.present(alert, animated: true, completion: nil)
         }
 
+        let op = AlertOperation(title: title, message: message, cancelButtonTitle: cancelButtonTitle, otherButtonTitles: otherButtonTitles, handler: handler, presentation: presentation)
+
+//        let op = OOperation { finished in
+//            let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//            let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .cancel) { alert in
+//                handler?(0)
+//                finished()
+//            }
+//            alert.addAction(cancelAction)
+//
+//            if let titles = otherButtonTitles {
+//                for (index, title) in titles.enumerated() {
+//                    let action = UIAlertAction(title: title, style: .default) { (alert) -> Void in
+//                        handler?(index + 1) // shifted by one because of 'cancel' action
+//                        finished()
+//                    }
+//                    alert.addAction(action)
+//                }
+//            }
+//            self.present(alert, animated: true, completion: nil)
+//
+//        }
+
         alertQueue.addOperation(op)
+        return op.alert
     }
 
 }
