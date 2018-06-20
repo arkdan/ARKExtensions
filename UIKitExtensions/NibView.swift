2e618b60a19da5c3052c834e1cc938548eae0520
arkadzi.daniyelian <arkadzi.daniyelian@accenture.com>
2018-06-20 14:59:15 +0300

    UICollectionViewCell awakeFromNib setup

--- UIKitExtensions/NibView.swift
+++ UIKitExtensions/NibView.swift
@@ -38,9 +38,18 @@ open class SetupCollectionCell: UICollectionViewCell {
         setup()
     }
 
+    // UICollectionViewCell outlets are still nil on init(coder: ). We need to call setup() on awakeFromNib
+    private var shouldSetupOnAwakeFromNib = false
     public required init?(coder aDecoder: NSCoder) {
+        shouldSetupOnAwakeFromNib = true
         super.init(coder: aDecoder)
-        setup()
+    }
+
+    open override func awakeFromNib() {
+        super.awakeFromNib()
+        if shouldSetupOnAwakeFromNib {
+            setup()
+        }
     }
 
     open func setup() {
